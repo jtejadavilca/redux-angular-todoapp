582357b7149676424d845c699d4cf01ee7158226
Jose Tejada <jtejadavilca@gmail.com>
2019-06-15 01:40:18 -0500

    Se implemenan acciones de editar y borrar TODOs

--- src/app/todo/todo-item/todo-item.component.ts
+++ src/app/todo/todo-item/todo-item.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { Todo } from '../model/todo.model';
+import { FormControl, Validators } from '@angular/forms';
+import { AppState } from '../../app.reducers';
+import { Store } from '@ngrx/store';
+import { ToggleTodoAction, EditarTodoAction, BorrarTodoAction } from '../todo.actions';
 
 @Component({
   selector: 'app-todo-item',
@@ -7,9 +12,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoItemComponent implements OnInit {
 
-  constructor() { }
+  @Input() todo: Todo;
+  @ViewChild('txtInputFisico') txtInputFisico: ElementRef;
+
+  chkField: FormControl;
+
+  txtInput: FormControl;
+
+  editando: boolean;
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.chkField = new FormControl(this.todo.completado);
+    this.txtInput = new FormControl(this.todo.texto, Validators.required);
+
+    this.chkField.valueChanges.subscribe(valor => {
+      const accion = new ToggleTodoAction(this.todo.id);
+      this.store.dispatch(accion);
+    });
   }
 
+  editar() {
+    this.editando = true;
+    setTimeout(() => {
+      this.txtInputFisico.nativeElement.select();
+    }, 1);
+  }
+
+  terminarEdicion() {
+    this.editando = false;
+
+    if (this.txtInput.invalid || this.txtInput.value === this.todo.texto) {
+      return;
+    }
+
+    const accion = new EditarTodoAction(this.todo.id, this.txtInput.value);
+    this.store.dispatch(accion);
+  }
+
+  borrarTodo() {
+    const accion = new BorrarTodoAction( this.todo.id );
+    this.store.dispatch(accion);
+  }
 }
--- src/app/todo/todo.reducer.ts
+++ src/app/todo/todo.reducer.ts
@@ -1,15 +1,47 @@
 import * as fromTodo from './todo.actions';
 import { Todo } from './model/todo.model';
 
+const todo1 = new Todo('Vencer a Thanos');
+const todo2 = new Todo('Salvar el mundo');
+const todo3 = new Todo('Pedir prestado el traje de Ironman');
 
-const estadoInicial: Todo[] = [];
+todo2.completado = true;
 
-export function todoReducer( state, action: fromTodo.Acciones ): Todo[] {
+const estadoInicial: Todo[] = [todo1, todo2, todo3];
+
+export function todoReducer( state = estadoInicial, action: fromTodo.Acciones ): Todo[] {
 
     switch ( action.type ) {
         case fromTodo.AGREGAR_TODO :
             const todo = new Todo( action.texto );
             return [ ...state, todo ];
+        case fromTodo.TOGGLE_TODO :
+
+            return state.map( todoEdit => {
+                if (todoEdit.id === action.id) {
+                    return {
+                        ...todoEdit,
+                        completado: !todoEdit.completado
+                    };
+                }
+
+                return todoEdit;
+            });
+        case fromTodo.EDITAR_TODO :
+            return state.map( todoEdit => {
+                if (todoEdit.id === action.id) {
+
+                    return {
+                        ...todoEdit,
+                        texto: action.texto
+                    };
+                }
+
+                return todoEdit;
+
+            });
+        case fromTodo.BORRAR_TODO :
+            return state.filter( todoEdit => todoEdit.id !== action.id );
         default :
             return state;
     }
